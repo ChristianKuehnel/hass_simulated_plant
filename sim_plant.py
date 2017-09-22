91a4218dea26e774ce34773e9768576fff9ce226
Christian Kühnel <christian.kuehnel@gmail.com>
2017-09-22 16:21:29 +0200

    improved stability when restarting hass during testing

--- sim_plant.py
+++ sim_plant.py
@@ -7,21 +7,24 @@ import time
 
 import paho.mqtt.client as mqtt
 
-client = mqtt.Client()
-client.connect('localhost', 1883, 60)
-
-
 while(True):
-  send_msg = {
-    'battery': random.randrange(80, 100, 1),
-    'temperature': random.randrange(0, 30, 1),
-    'brightness': random.randrange(40, 100, 1),
-    'moisture': random.randrange(10, 70, 1),
-    'conductivity': random.randrange(40, 70, 1),
-    'timestamp': datetime.now().isoformat(),
-  }
-  client.publish('test/simulated_plant', payload=json.dumps(send_msg), qos=0, retain=False)
-  print(send_msg)
-  time.sleep(10)
+  try:
+    client = mqtt.Client()
+    client.connect('localhost', 1883, 60)
+  
+    send_msg = {
+      'battery': random.randrange(1, 100, 1),
+      'temperature': random.randrange(0, 30, 1),
+      'brightness': random.randrange(0, 5000, 1),
+      'moisture': random.randrange(0, 100, 1),
+      'conductivity': random.randrange(0, 5000, 1),
+      'timestamp': datetime.now().isoformat(),
+      }
+    client.publish('test/simulated_plant', payload=json.dumps(send_msg), qos=0, retain=False)
+    client.disconnect()
+    print(send_msg)
+  except ConnectionRefusedError:
+    print('not connected')
 
+  time.sleep(10)
 
